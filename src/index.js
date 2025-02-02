b6873bd6c6abfb19cd2705512c1c35df5478a2a7
SvitlanaLucky <svitlanalucky04@gmail.com>
2025-02-02 20:15:08 +0200

    second

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log('hello');
+const refs = {
+  input: document.querySelector('.input'),
+  btn: document.querySelector('.btn'),
+};
+
+refs.input.addEventListener('click', onInputClick);
+refs.btn.addEventListener('click', onBtnClick);
+
+function onInputClick() {}
+function onBtnClick() {}
